52e02a3c1b0c9614c3a1ca88101734bbeba6af43
Caldzek <caldzudiswojciech@gmail.com>
2022-02-15 21:35:30 +0100

    y

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js")
+const { MembershipStates } = require("discord.js/typings/enums")
 require("dotenv").config()
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS",
     ]
 })
 
@@ -16,6 +18,12 @@ client.on("messageCreate", (message) => {
     if (message.content == "Hi"){
         message.reply("Hello my enjoyer! ♥")
     }
+})
+
+const welcomeChannelId = "943243536608927785"
+
+client.on("guildMemberAdd", (meber) => {
+    member.guild.channels.cache.get(welcomeChannelId).send('<@${member.id}> Welcome to the server!')
 })
 
 client.login(process.env.TOKEN)
