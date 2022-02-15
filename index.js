4ccb58cf2ba77584706838ac879c02ec1279eaef
Caldzek <caldzudiswojciech@gmail.com>
2022-02-15 21:45:39 +0100

    p

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const Discord = require("discord.js")
-const { MembershipStates } = require("discord.js/typings/enums")
 require("dotenv").config()
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES",
-        "GUILD_MEMBERS",
+        "GUILD_MEMBERS"
     ]
 })
 
 client.on("ready", () => {
-    console.log('Logged in as ${client.user.tag}')
+    console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "Hi"){
-        message.reply("Hello my enjoyer! ♥")
+    if (message.content == "hi"){
+        message.reply("Hello World!")
     }
 })
 
 const welcomeChannelId = "943243536608927785"
 
-client.on("guildMemberAdd", (meber) => {
-    member.guild.channels.cache.get(welcomeChannelId).send('<@${member.id}> Welcome to the server!')
+client.on("guildMemberAdd", async (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+    })
 })
 
 client.login(process.env.TOKEN)
