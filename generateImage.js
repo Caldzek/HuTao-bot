8580b3ed49f6fc8bae3d075b3b96552cc5e4a408
Caldzek <caldzudiswojciech@gmail.com>
2022-02-17 19:03:51 +0100

    y

--- generateImage.js
+++ generateImage.js
@@ -1,6 +1,6 @@
 const Canvas = require("canvas")
 const Discord = require("discord.js")
-const background = "https://i.imgur.com/zvWTUVu.jpg"
+const background = "https://wallpaperaccess.com/full/6996556.jpg"
 
 const dim = {
     height: 675,
@@ -46,15 +46,15 @@ const generateImage = async (member) => {
     ctx.textAlign = "center"
 
     // draw in Welcome
-    ctx.font = "50px Roboto"
+    ctx.font = "50px"
     ctx.fillText("Welcome", dim.width/2, dim.margin + 70)
 
     // draw in the username
-    ctx.font = "60px Roboto"
+    ctx.font = "60px"
     ctx.fillText(username + discrim, dim.width/2, dim.height - dim.margin - 125)
 
     // draw in to the server
-    ctx.font = "40px Roboto"
+    ctx.font = "40px"
     ctx.fillText("to the server", dim.width / 2, dim.height - dim.margin - 50)
 
     const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-// const generateImage = require("./generateImage")
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
@@ -11,48 +11,24 @@ const client = new Discord.Client({
     ]
 })
 
-let bot = {
-    client, 
-    prefix: "n.",
-    owners: ["315850603396071424"]
-}
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
-client.commands = new Discord.Collection()
-client.events = new Discord.Collection()
-client.slashcommands = new Discord.Collection()
-client.buttons = new Discord.Collection()
+client.on("messageCreate", (message) => {
+    if (message.content == "Hi"){
+        message.reply("Hello my enjoyer! <3")
+    }
+})
 
-client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
-client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
-client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
-client.loadButtons = (bot, reload) => require("./handlers/buttons")(bot, reload)
+const welcomeChannelId = "943911602439196744"
 
-client.loadEvents(bot, false)
-client.loadCommands(bot, false)
-client.loadSlashCommands(bot, false)
-client.loadButtons(bot, false)
-
-
-module.exports = bot
-
-// client.on("ready", () => {
-//     console.log(`Logged in as ${client.user.tag}`)
-// })
-
-// client.on("messageCreate", (message) => {
-//     if (message.content == "hi"){
-//         message.reply("Hello World!")
-//     }
-// })
-
-// const welcomeChannelId = "926530810008453120"
-
-// client.on("guildMemberAdd", async (member) => {
-//     const img = await generateImage(member)
-//     member.guild.channels.cache.get(welcomeChannelId).send({
-//         content: `<@${member.id}> Welcome to the server!`,
-//         files: [img]
-//     })
-// })
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
 
 client.login(process.env.TOKEN)
