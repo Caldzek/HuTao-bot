81df4a11d911a07e0f3bf4769f8d090aa32dc46c
Caldzek <caldzudiswojciech@gmail.com>
2022-02-17 18:48:32 +0100

    y

--- commands/test/rolesselector.js
+++ commands/test/rolesselector.js
@@ -7,11 +7,11 @@ module.exports = {
     run: async ({client, message, args}) => {
         message.channel.send({
             embeds: [
-                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("BLUE")
+                new MessageEmbed().setTitle("Select Role").setDescription("Select roles from the buttons below").setColor("RED")
             ],
             components: [
                 new MessageActionRow().addComponents([
-                    new MessageButton().setCustomId("role-935243920177242122").setStyle("PRIMARY").setLabel("5")
+                    new MessageButton().setCustomId("role-943221231954591815").setStyle("PRIMARY").setLabel("SuperAdmin")
                 ])
             ]
         })
